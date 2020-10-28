562c3b8fa1628ac4656c3ec45ddcf44b64f46ec8
mrdrummy93 <vasya_2393@drummer.ru>
2020-10-28 22:59:29 +0300

    fix: fix count cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,13 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats( matrix ) {
-    throw new CustomError('Not implemented');
-        const newMatrix = matrix.flat();
-        let result = 0;
-        newMatrix.forEach(el => {
-               if (el === '^^') {
-                       result += 1;
-               }
+    let result = 0;
+        matrix.forEach(array => {
+            array.forEach(el => {
+                if (el === '^^') {
+                    result += 1;
+                }
+            })
        })
         return result;
 }
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
+module.exports = function createDreamTeam( members ) {
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (members !== '') {
+    return false
+  } else {
+   return members.sort().toString().slice(1);
+  }
 };
