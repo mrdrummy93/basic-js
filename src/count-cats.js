a25e137558e984c8f4075bbbe7c402a2f142067c
mrdrummy93 <vasya_2393@drummer.ru>
2020-10-28 13:24:06 +0300

    init: finish carbon dating task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats( matrix ) {
+    throw new CustomError('Not implemented');
         const newMatrix = matrix.flat();
         let result = 0;
         newMatrix.forEach(el => {
